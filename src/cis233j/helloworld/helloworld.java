2e27f53e839bfcfde81ed04a93811d16d300c850
Michael Flynn <michael.flynn4@pcc.edu>
2017-10-22 17:12:10 -0600

    Added date display to the main method

--- src/cis233j/helloworld/helloworld.java
+++ src/cis233j/helloworld/helloworld.java
@@ -1,7 +1,10 @@
 package cis233j.helloworld;
 
+import java.util.Date;
+
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println(new Date());
     }
 }
