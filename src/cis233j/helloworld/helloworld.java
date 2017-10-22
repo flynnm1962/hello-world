5e13fe751a8511d2eb0361cc40e2b1a9edb3bbc9
Michael Flynn <michael.flynn4@pcc.edu>
2017-10-22 17:24:42 -0600

    Added my name and class description

--- src/cis233j/helloworld/helloworld.java
+++ src/cis233j/helloworld/helloworld.java
@@ -2,6 +2,10 @@ package cis233j.helloworld;
 
 import java.util.Date;
 
+/**
+ * A simple class to print a message and a date to the screen
+ * Created by Michael S. Flynn on 10/22/2017
+ */
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
